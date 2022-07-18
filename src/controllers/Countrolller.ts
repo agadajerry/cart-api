4fe21b1367d6f5fb675f844ff2eb89b04de557e4
Idoko Agada Jerry <Idokoidoko4@gmail.com>
2022-07-18 11:13:05 +0100

    staff update

--- src/controllers/Countrolller.ts
+++ src/controllers/Countrolller.ts
@@ -41,11 +41,24 @@ export class Countrollers {
   }
 
   async getCart(req: Request, res: Response) {
-    const result = await cartModel.findAll();
-   //  getCartTotalPriceAndLength() 
+   const {code} =  req.query;
 
-   console.log(result.length)
-    return res.send({ msg: "List of products in cart", result });
+   console.log(code)
+
+   let totalPrice:number = 0;
+    const result:any = await cartModel.findAll();
+
+   for(let i = 0; i < result.length;i++){
+      totalPrice+= result[i].cartProductPrice;
+   }
+  const discount = await applyCouponCode(code,totalPrice,result.length);
+  
+
+      // const discount =  getCartTotalPriceAndLength(totalPrice,result.length, req.query.code);
+
+      console.log(discount)
+
+    return res.send({ msg: "List of products in cart", result, discount:discount });
   }
 
   async findOneCouponCode(req: Request, res: Response) {
@@ -62,19 +75,21 @@ export class Countrollers {
 
       if (coupon) {
 
-         switch(coupon_code){
-             case "FIXED10":
-               return applyCouponCode("FIXED10");
-               case "PERCENT10":
-               return applyCouponCode("PERCENT10");
-               case "MIXED10":
-               return applyCouponCode("MIXED10");
-               case "REJECTED10":
-               return applyCouponCode("REJECTED10");
-               default:
-               return res.send({ error: "No coupon code found or expired"});
+         
+         // switch(coupon_code){
+         //     case "FIXED10":
+         //       return applyCouponCode("FIXED10");
+         //       case "PERCENT10":
 
-         }
+         //       return applyCouponCode("PERCENT10");
+         //       case "MIXED10":
+         //       return applyCouponCode("MIXED10");
+         //       case "REJECTED10":
+         //       return applyCouponCode("REJECTED10");
+         //       default:
+         //       return res.send({ error: "No coupon code found or expired"});
+
+         // }
       } else {
         return res.send({ error: "No coupon code found or expired" });
       }
@@ -86,8 +101,53 @@ export class Countrollers {
 
 // apply coupon code
 
-function applyCouponCode(coupon: string) {
-  return coupon;
+ async function applyCouponCode(coupon: any, totalPrice:number, cartSize:number) {
+
+  
+   const couponValidity = await couponTable.findOne({
+      where: {
+        coupon_code: coupon,
+        coupon_expiry_date: { [Op.gte]: new Date() },
+      },
+      attributes: ["coupon_code", "couponType"],
+    });
+
+    return couponValidity;
+   //  if(couponValidity){
+   //    switch(coupon){
+   //      case "FIXED10":
+   //       if(cartSize >= 1 && totalPrice > 50){
+   //          return totalPrice - 10
+   //       }
+   //      case "PERCENT10":
+   //      if(cartSize >= 2 && totalPrice > 100){
+   //          return totalPrice * 0.10
+   //       }
+        
+   //      case "MIXED10":
+   //       if(cartSize >= 3 && totalPrice >= 200){
+   //          return totalPrice * 0.10
+   //       }
+   //      case "REJECTED10":
+   //      if( totalPrice > 1000){
+   //          return totalPrice * 0.10
+         
+   //      }
+
+   //      default:
+   //      return 0;
+   //    }
+   //  }
 }
+
+function  getCartTotalPriceAndLength(totalPrice:number,cartSize:number,code:any){
+
+   if(code == "FIXED10" && totalPrice > 50  && cartSize > 1){
+
+      return totalPrice - 10
+   }
+  
+}
+
 
 export default new Countrollers();
